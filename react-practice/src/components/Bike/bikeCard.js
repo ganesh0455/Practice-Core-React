0b5f333e48684900a1766ceb2ba0d02395c58196
Ganesh V <ganesh.c@veltris.com>
2024-04-22 00:28:01 +0530

    minor one

--- react-practice/src/components/Bike/bikeCard.js
+++ react-practice/src/components/Bike/bikeCard.js
@@ -4,8 +4,8 @@ import { BIKE_IMAGE } from '../../constants';
 const BikeCard = ({bike}) => {
     return (
         <Link className='bike-card-container' to={`/bikeDetails/${bike.id}`}>
-            <img className='bike-card-image' src={BIKE_IMAGE} alt="bike-img" />
-            <h3 className='bike-card-heading'>{bike.bike_image_1}</h3>
+            <img className='bike-card-image' src={bike.bike_image_1} alt="bike-img" />
+            <h3 className='bike-card-heading'>{bike.bike_name}</h3>
         </Link>
     )
 }
